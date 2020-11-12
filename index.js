e227b8bf5013afc5105e79e507b488d14f59deb9
Gretchen Krammes <gkkrammes@gmail.com>
2020-11-11 20:32:23 -0500

    add employee function added

--- index.js
+++ index.js
@@ -85,3 +85,96 @@ function viewEmployees() {
             }
         })
 }
+
+function addEmployee() {
+    inquirer
+        .prompt(
+            {
+                type: "list",
+                name: "add",
+                message: "What would you like to add?",
+                choices: [
+                    "department",
+                    "roles",
+                    "employee"
+                ]
+            })
+        .then(function (answer) {
+            switch (answer.add) {
+                case ("department"):
+                    inquirer.prompt(
+                        {
+                            type: "input",
+                            name: "department_add",
+                            message: "What department would you like to add?"
+                        }
+                    )
+                    .then(function (answer) {
+                        connection.query('INSERT INTO department (department_name) VALUES ("${answer.department_add}");', function(err, res) {
+                            if (err) throw err;
+                        })
+                        console.log("Department Added")
+                        prompt()
+                    })
+                    break;
+                case ("roles"):
+                    inquirer.prompt(
+                        {
+                            type: "input",
+                            name: "title",
+                            message: "What is the title of the role?"
+                        },
+                        {
+                            type: "input",
+                            name: "salary",
+                            message: "What is the salary for this role?"
+                        },
+                        {
+                            type: "input",
+                            name: "department_id",
+                            message: "What is the department id for this role?"
+                        }
+                    )
+                    .then(function (answer) {
+                        connection.query('INSERT INTO roles (title, salary, department_id) VALUES ("${answer.title}", "${answer.salary}", "${answer.department_id}");', function(err, res) {
+                            if (err) throw err;
+                        })
+                        console.log("Role Added")
+                        prompt()
+                    })
+                    break;
+                case ("roles"):
+                    inquirer.prompt(
+                        {
+                            type: "input",
+                            name: "first_name",
+                            message: "What is the employee's first name?"
+                        },
+                        {
+                            type: "input",
+                            name: "last_name",
+                            message: "What is the employee's last name?"
+                        },
+                        {
+                            type: "input",
+                            name: "role_id",
+                            message: "What is the employee's role id?"
+                        },
+                        {
+                            type: "input",
+                            name: "manager_id",
+                            message: "What is the employee's manager id?"
+                        }
+                    )
+                    .then(function (answer) {
+                        connection.query('INSERT INTO roles (first_name, last_name, role_id, manager_id) VALUES ("${answer.first_name}", "${answer.last_name}", "${answer.role_id}", "${answer.manager_id}");', function(err, res) {
+                            if (err) throw err;
+                        })
+                        console.log("Employee Added")
+                        prompt()
+                    })
+                    break;
+        }
+
+    })
+}
