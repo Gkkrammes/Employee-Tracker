b7f53d86531a4a23370bd6da0c24906f642e737f
Gretchen Krammes <gkkrammes@gmail.com>
2020-12-11 15:47:02 -0500

    rework started

--- index.js
+++ index.js
@@ -9,172 +9,53 @@ const connection = mysql.createConnection({
     database: "employee_db"
 });
 
-connection.connect((err) => {
+connection.connect(function(err) {
     if (err) throw err;
-    begin();
-})
+    prompt();
+});
 
-function begin() {
+function prompt() {
     inquirer
         .prompt({
-            type: "list",
             name: "action",
+            type: "list",
             message: "What task would you like to complete?",
             choices: [
-                "View All Employees",
-                "Add Employee",
-                "Remove Employee",
-                "Update Employee",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Update employee",
                 
             ]
         })
         .then(function (answer) {
-            switch (answer.main) {
-                case ("View"):
-                    viewEmployees();
-                    break;
-                case ("Add Employee"):
-                    addEmployee();
-                    break;
-                case ("Remove Employee"):
-                    removeEmployee();
-                    break;
-                case ("Update Employee"):
-                    updateRole();
-                    break;
-                }
-        })};
-begin()
-
-function viewEmployees() {
-    inquirer
-        .prompt(
-            {
-                type: "list",
-                name: "view",
-                message: "How would you like to view the employee?",
-                choices: [
-                    "by name",
-                    "by role",
-                    "by department",
-                ]
-            })
-        .then(function (answer) {
-            switch (answer.view) {
-                case ("name"):
-                    connection.query('SELECT * FROM name', function (err, res) {
-                        if (err) throw err;
-                        console.table(res)
-                        prompt()
-                    })
-                    break;
-                case ("role"):
-                    connection.query('SELECT * FROM role', function (err, res) {
-                        if (err) throw err;
-                        console.table(res)
-                        prompt()
-                    })
-                    break;
-                case ("department"):
-                    connection.query('SELECT * FROM department', function (err, res) {
-                        if (err) throw err;
-                        console.table(res)
-                        prompt()
-                    })
-                    break;
+            if (answer.action === 'Add a department') {
+                addDepartment();
             }
-        })
-}
-
-function addEmployee() {
-    inquirer
-        .prompt(
-            {
-                type: "list",
-                name: "add",
-                message: "What would you like to add?",
-                choices: [
-                    "department",
-                    "roles",
-                    "employee"
-                ]
-            })
-        .then(function (answer) {
-            switch (answer.add) {
-                case ("department"):
-                    inquirer.prompt(
-                        {
-                            type: "input",
-                            name: "department_add",
-                            message: "What department would you like to add?"
-                        }
-                    )
-                    .then(function (answer) {
-                        connection.query('INSERT INTO department (department_name) VALUES ("${answer.department_add}");', function(err, res) {
-                            if (err) throw err;
-                        })
-                        console.log("Department Added")
-                        prompt()
-                    })
-                    break;
-                case ("roles"):
-                    inquirer.prompt(
-                        {
-                            type: "input",
-                            name: "title",
-                            message: "What is the title of the role?"
-                        },
-                        {
-                            type: "input",
-                            name: "salary",
-                            message: "What is the salary for this role?"
-                        },
-                        {
-                            type: "input",
-                            name: "department_id",
-                            message: "What is the department id for this role?"
-                        }
-                    )
-                    .then(function (answer) {
-                        connection.query('INSERT INTO roles (title, salary, department_id) VALUES ("${answer.title}", "${answer.salary}", "${answer.department_id}");', function(err, res) {
-                            if (err) throw err;
-                        })
-                        console.log("Role Added")
-                        prompt()
-                    })
-                    break;
-                case ("roles"):
-                    inquirer.prompt(
-                        {
-                            type: "input",
-                            name: "first_name",
-                            message: "What is the employee's first name?"
-                        },
-                        {
-                            type: "input",
-                            name: "last_name",
-                            message: "What is the employee's last name?"
-                        },
-                        {
-                            type: "input",
-                            name: "role_id",
-                            message: "What is the employee's role id?"
-                        },
-                        {
-                            type: "input",
-                            name: "manager_id",
-                            message: "What is the employee's manager id?"
-                        }
-                    )
-                    .then(function (answer) {
-                        connection.query('INSERT INTO roles (first_name, last_name, role_id, manager_id) VALUES ("${answer.first_name}", "${answer.last_name}", "${answer.role_id}", "${answer.manager_id}");', function(err, res) {
-                            if (err) throw err;
-                        })
-                        console.log("Employee Added")
-                        prompt()
-                    })
-                    break;
-        }
-
-    })
-}
+            else if(answer.action === 'Add a role') {
+                addRole();
+            }
+            else if(answer.action === 'Add an employee') {
+                addEmployee();
+            }
+            else if(answer.action === 'View all departments') {
+                viewDepartments();
+            }
+            else if(answer.action === 'View all roles') {
+                viewRoles();
+            }
+            else if(answer.action === 'View all employees') {
+                viewEmployees();          
+            }
+            else if(answer.action === "Update employee") {
+                updateEmployee();
+            }     
+        
+            else{
+              connection.end();
+            }
+          });
+    }
--- schema.sql
+++ schema.sql
@@ -5,24 +5,48 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(30) NOT NULL, 
-    PRIMARY KEY(id)
+		id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-    id INT AUTO_INCREMENT NOT NULL,
+		id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
-    PRIMARY KEY(id)
+    department_id INT NOT NULL
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL, 
+		id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT,
-    PRIMARY KEY(id)
+    manager_id INT
 );
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kellen", "Frank", 100, 150);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Marie", "Denise", 200, 250 );
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kyrie", "Woolf", 300, 350);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Collins", "Lewis", 400, 450);
+
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (100, "Data Analyst", 70, 10);
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (200, "Human Resources Manager", 50, 20);
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (300, "Engineer", 85, 30);
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (400, "CFO", 12.5, 40);
+
+INSERT INTO department (id, department_name)
+VALUES (10, "Information Technology");
+INSERT INTO department (id, department_name)
+VALUES (20, "Human Resources");
+INSERT INTO department (id, department_name)
+VALUES (30, "Engineering");
+INSERT INTO department (id, department_name)
+VALUES (40, "Finance");
