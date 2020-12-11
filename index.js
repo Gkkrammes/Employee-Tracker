67f54e41ea69f41dff43bce5615d4c7e7b236e78
Gretchen Krammes <gkkrammes@gmail.com>
2020-12-11 17:17:15 -0500

    final edits and such

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ connection.connect(function(err) {
 });
 
 function prompt() {
-    inquirer
-        .prompt({
+    inquirer.prompt({
             name: "action",
             type: "list",
             message: "What task would you like to complete?",
@@ -32,30 +31,203 @@ function prompt() {
             ]
         })
         .then(function (answer) {
-            if (answer.action === 'Add a department') {
-                addDepartment();
-            }
-            else if(answer.action === 'Add a role') {
-                addRole();
-            }
-            else if(answer.action === 'Add an employee') {
-                addEmployee();
-            }
-            else if(answer.action === 'View all departments') {
-                viewDepartments();
-            }
-            else if(answer.action === 'View all roles') {
-                viewRoles();
-            }
-            else if(answer.action === 'View all employees') {
-                viewEmployees();          
-            }
-            else if(answer.action === "Update employee") {
-                updateEmployee();
-            }     
-        
-            else{
-              connection.end();
-            }
-          });
-    }
+            switch (answer.action) {
+                case ("Add a department"):
+                    addDept();
+                    break;
+                case ("Add a role"):
+                    addRole();
+                    break;
+                case ("Add an employee"):
+                    addEmployee();
+                    break;
+                case ("View all departments"):
+                    viewDepartments();
+                    break;
+                case ("View all roles"):
+                    viewRoles();
+                    break;
+                case ("View all employees"):
+                    viewEmployees();
+                    break;
+                case ("Update Employee"):
+                    updateEmployee();
+                    break;
+            };
+        })
+    };
+
+    function addDept() {
+        inquirer
+        .prompt([
+        {
+          name: "deptID",
+          type: "input",
+          message: "What is the ID of the new department?",
+        },
+        {
+        name: "deptName",
+        type: "input",
+        message: "What is the name of the new department?",
+        }
+        ])
+  
+        .then(function(response) {
+            connection.query("INSERT INTO departments SET ?", {
+                    id: response.deptID,
+                    department_name: response.deptName,
+            },
+            function(err) {
+                if (err) throw err;
+                console.log("Your department was created successfully!");
+                prompt();
+                }
+            );
+        });
+    };
+
+    function addRole() {
+        inquirer
+        .prompt([
+        {
+          name: "roleID",
+          type: "input",
+          message: "What is the ID of the new role?",
+        },
+        {
+        name: "roleTtile",
+        type: "input",
+        message: "What is the title of the new role?",
+        },
+        {
+        name: "roleSalary",
+        type: "input",
+        message: "What is the salary of the new role?",
+        },
+        {
+        name: "roleDepartment",
+        type: "input",
+        message: "What is the department ID of the new role?",
+        }
+        ])
+  
+        .then(function(response) {
+            connection.query("INSERT INTO roles SET ?", {
+                    id: response.roleID,
+                    title: response.roleTtile,
+                    salary: response.roleSalary,
+                    department_id: response.roleDepartment,
+            },
+            function(err) {
+                if (err) throw err;
+                console.log("Your new role was created successfully!");
+                prompt();
+                }
+            );
+        });
+    };
+
+    function addEmployee() {
+        inquirer
+        .prompt([
+        {
+          name: "employeeID",
+          type: "input",
+          message: "What is the ID of the new employee?",
+        },
+        {
+        name: "empFirstName",
+        type: "input",
+        message: "What is the first name of the new employee?",
+        },
+        {
+        name: "empLastName",
+        type: "input",
+        message: "What is the last name of the new employee?",
+        },
+        {
+        name: "empRole",
+        type: "input",
+        message: "What is the role ID for the new employee?",
+        },
+        {
+        name: "empManager",
+        type: "input",
+        message: "What is id of the new employee's manager?",
+        }
+        ])
+  
+        .then(function(response) {
+            connection.query("INSERT INTO employee SET ?", {
+                    id: response.employeeID,
+                    first_name: response.empFirstName,
+                    last_name: response.empLastName,
+                    role_id: response.empRole,
+                    manager_id: response.empManager,
+            },
+            function(err) {
+                if (err) throw err;
+                console.log("Your new employee was created successfully!");
+                prompt();
+            });
+        });
+    };
+
+    function viewDepartments() {
+        let query = "SELECT * FROM  departments";
+        connection.query(query, function(err, res) {
+                console.table(res);
+                prompt();
+        });
+    };
+
+    function viewEmployees() {
+        let query = "SELECT * FROM  employee";
+        connection.query(query, function(err, res) {
+            console.table(res);
+            prompt();
+        });
+    };
+
+    function viewRoles() {
+        let query = "SELECT * FROM  roles";
+        connection.query(query, function(err, res) {
+            console.table(res);
+            prompt();
+        });
+    };
+
+    function updateEmployee() {
+            let choices = [];
+    
+            connection.query("SELECT first_name FROM  employee", function(err, res) {
+               res.forEach(row => {
+                   choices.push(row.first_name);
+                   return choices;
+               });
+            
+            inquirer
+            .prompt([{
+              name: "employeeName",
+              type: 'list',
+              message: "Which employee you would like to update?",
+              choices: choices
+             },
+            {
+            name: "empNewRole",
+            type: "input",
+            message: "What is the new role ID of the employee?",
+            }])
+                .then(function(response) {
+                    connection.query(
+                        `UPDATE employee SET role_id = ${response.empNewRole} WHERE first_name = "${response.employeeName}"`, {
+                    },
+                    function(err) {
+                        if (err) throw err;
+                        console.log("Employee Updated!");
+                        prompt();
+                    }
+                )}
+            )}
+    )};
+    
--- schema.sql
+++ schema.sql
@@ -5,24 +5,35 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-		id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    department_name VARCHAR(30) NOT NULL
+		id INT AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-		id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+		id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-		id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
+		id INT AUTO_INCREMENT NOT NULL, 
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT
+    manager_id INT NOT NULL,
+    PRIMARY KEY (id)
 );
+
+SELECT department.id, roles.department_id 
+FROM  department
+INNER JOIN roles ON department.id=roles.department_id;
+
+SELECT employee.role_id, roles.id
+FROM employee
+INNER JOIN roles ON employee.role_id=roles.id;
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Kellen", "Frank", 100, 150);
